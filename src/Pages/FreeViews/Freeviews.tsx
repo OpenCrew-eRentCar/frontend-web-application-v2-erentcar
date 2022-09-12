f4a4b0c44452019f829acce88c053caf2bc8ba23
Sebastian Bran <sebastianbranzapata@gmail.com>
2022-09-11 22:33:20 -0500

    feat: add login and register dialog ( #2, #3 )

--- src/Pages/FreeViews/Freeviews.tsx
+++ src/Pages/FreeViews/Freeviews.tsx
@@ -1,12 +1,38 @@
+import { useState } from "react";
 import Header from "../../Components/Header";
+import { Dialog } from "primereact/dialog";
+import LoginForm from "./Components/LoginForm";
+import RegisterForm from "./Components/RegisterForm";
 
 export const Freeviews = () => {
+  const [displayAuthForm, setDisplayAuthForm] = useState(false);
+  const [displayLoginForm, setDisplayLoginForm] = useState(true);
+
+  const onClickLoginButton = () => {
+    setDisplayAuthForm(true);
+  };
+
   return (
     <div>
-      <Header authenticated={false}/>
+      <Header authenticated={false} onClickLoginButton={onClickLoginButton} />
       <h1>Freeviews</h1>
+      <Dialog visible={displayAuthForm} onHide={() => setDisplayAuthForm(false)}>
+        {displayLoginForm ? (
+          <LoginForm
+            displayAuthForm={displayAuthForm}
+            setDisplayAuthForm={setDisplayAuthForm}
+            setDisplayLoginForm={setDisplayLoginForm}
+          />
+        ) : (
+          <RegisterForm
+            displayAuthForm={displayAuthForm}
+            setDisplayAuthForm={setDisplayAuthForm}
+            setDisplayLoginForm={setDisplayLoginForm}
+          />
+        )}
+      </Dialog>
     </div>
   );
-}
+};
 
 export default Freeviews;
