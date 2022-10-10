c7a1ce8191494bdffcba77f88c62d0c90fc4854b
Sebastian Bran <sebastianbranzapata@gmail.com>
2022-10-10 17:26:34 -0500

    feat: refactor image and favorite button ( #6 )

--- src/Pages/AuthViews/Favourites/Components/FavoriteCard.tsx
+++ src/Pages/AuthViews/Favourites/Components/FavoriteCard.tsx
@@ -2,24 +2,21 @@ import { Button } from "primereact/button";
 import Favourite from "../../../../Models/Favourite.model";
 
 type Props = {
-  favs: Favourite,
-  deleteData: (id: number) => void
-}
+  favs: Favourite;
+  deleteData: (id: number) => void;
+};
 
 function FavoriteCard(props: Props) {
-
   return (
     <div className="bg-card-content flex max-w-[400px] md:max-w-[600px] md:h-[300px] rounded-[15px] shadow-sm mx-auto mt-[35px]">
       <div className=" md:w-[476px] md:flex border-r-2 border-gray-300 md:border-r-0">
-        <div className="md:w-[220px] bg-card-image rounded-l-[15px] content-center">
-          <div className="hidden md:flex justify-end pr-[16px] pt-[16px] h-[50px]">
-            <button onClick={() => props.deleteData(props.favs.id)}>
-              <i className="pi pi-heart-fill" style={{ fontSize: "2em" }}></i>
-            </button>
-          </div>
-          <div className="pt-[25px]">
-            <img src={props.favs.car.imagePath} />
-          </div>
+        <div className="lg:w-[220px] bg-primary rounded-l-lg flex relative">
+          <img alt="car" src={props.favs.car.imagePath} className="my-auto" />
+          <Button
+            icon="pi pi-heart"
+            className="!absolute !right-0 text-center p-button-rounded p-button-text !text-black !w-[50px] !bg-[#CEE4FF] hover:!bg-cyan-100"
+            onClick={() => props.deleteData(props.favs.id)}
+          />
         </div>
         <div className="md:w-[256px] md:border-r-2 border-gray-300 px-[12px] py-[15px]">
           <p className="text-[20px] font-bold">
@@ -49,11 +46,7 @@ function FavoriteCard(props: Props) {
         </div>
       </div>
       <div className="md:max-w-[124px] px-1 py-4 md:grid md:content-end">
-        <div className="flex md:hidden justify-center pt-[16px] h-[50px]">
-          <i className="pi pi-heart-fill" style={{ fontSize: "2em" }}></i>
-        </div>
         <div>
-
           <div className="pt-[200px]">
             <p className="text-[20px] font-bold">
               S/ {props.favs.car.rentAmountDay}
@@ -70,6 +63,6 @@ function FavoriteCard(props: Props) {
       </div>
     </div>
   );
-};
+}
 
 export default FavoriteCard;
