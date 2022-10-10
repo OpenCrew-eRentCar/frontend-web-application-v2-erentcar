6cba4f5041609b8a45db35651e7720dbac76ccc7
Sebastian Bran <sebastianbranzapata@gmail.com>
2022-10-10 17:17:15 -0500

    fix: refactor responsive

--- src/Pages/AuthViews/Rents/Rents.tsx
+++ src/Pages/AuthViews/Rents/Rents.tsx
@@ -1,32 +1,31 @@
-import {useEffect, useState} from "react";
-import RentsService from "../../../Services/Rent.service"
+import { useEffect, useState } from "react";
+import RentsService from "../../../Services/Rent.service";
 import RentEntity from "../../../Models/Rent.model";
-import Rent from "./Rent";
+import Rent from "./Components/Rent";
 
 export const Rents = () => {
-  const [rents, setRents] = useState([] as RentEntity[])
+  const [rents, setRents] = useState([] as RentEntity[]);
 
   useEffect(() => {
     const fetchRents = async () => {
       await RentsService.getRentsByToken()
-          .then(response => {
-              console.log(response)
-            setRents(response?.data.content as RentEntity[]);
-          })
-          .catch(error => {
-            console.log(error)
-          })
-    }
-    fetchRents()
-  }, [])
+        .then((response) => {
+          setRents(response?.data.content as RentEntity[]);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    fetchRents();
+  }, []);
 
   return (
-      <div className="w-full mx-auto px-3 lg:p-0 border-box lg:border-content">
-          {rents.map((rent) => (
-              <Rent key={rent.id} rent={rent} />
-          ))}
-      </div>
+    <div className="w-full mx-auto max-w-[830px] mt-5 px-3 lg:p-0 border-box lg:border-content">
+      {rents.map((rent) => (
+        <Rent key={rent.id} rent={rent} />
+      ))}
+    </div>
   );
-}
+};
 
 export default Rents;
